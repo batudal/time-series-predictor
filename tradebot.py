ac83d729e72e93b64096e1cf1d01b26dcaf5d4c2
Batu Dal <batudal@gmail.com>
2020-01-30 15:14:33 +0300

    added bollinger

--- tradebot.py
+++ tradebot.py
@@ -6,6 +6,7 @@ import numpy as np
 from binance.client import Client
 import os
 import matplotlib.pyplot as plt
+import statistics
 
 # binance setup
 binance_api_key = os.environ.get("BINANCE_KEY")
@@ -18,13 +19,14 @@ updater = Updater(telegram_token, use_context=True)
 bot = updater.bot
 job = updater.job_queue
 
-ma_1_interval = 7
+ma_1_interval = 5
 ma_2_interval = 14
+bollinger_interval = 9
 
 # first state
 prices = []
 history = []
-for kline in client.get_historical_klines_generator("BTCUSDT", Client.KLINE_INTERVAL_30MINUTE, "10 hour ago UTC"):
+for kline in client.get_historical_klines_generator("BTCUSDT", Client.KLINE_INTERVAL_15MINUTE, "1 hour ago UTC"):
     float_result = float(kline[4])
     prices.append(float_result)
 ma_1 = sum(prices[-ma_1_interval:])/ma_1_interval    
@@ -53,6 +55,9 @@ def algo_bot (update):
     global tradeCount
     global wallet_at_buy
     global wallet_at_sell
+    bollinger_ma_list = []
+    bollinger_std_list = []
+    bollinger_sum = 0
     
     markets = client.get_all_tickers()
     info = client.get_account()
@@ -64,16 +69,42 @@ def algo_bot (update):
     
     # get prices
     prices = []
-    for kline in client.get_historical_klines_generator("BTCUSDT", Client.KLINE_INTERVAL_30MINUTE, "10 hour ago UTC"):
+    for kline in client.get_historical_klines_generator("BTCUSDT", Client.KLINE_INTERVAL_15MINUTE, "12 hour ago UTC"):
         float_result = float(kline[4])
         prices.append(float_result)
+    
+    pricesLength = len(prices)-1
+    
+    for i in range(pricesLength, (pricesLength - bollinger_interval*2),-1):
+        bollinger_sum = sum(prices[(i-bollinger_interval):i])/bollinger_interval
+        bollinger_ma_list.append(bollinger_sum)
         
+    bollinger_std_list.append(statistics.stdev(bollinger_ma_list))
+    
+    print(bollinger_ma_list)
+    print(bollinger_std_list)
+    
     ma_1 = sum(prices[-ma_1_interval:])/ma_1_interval    
     ma_2 = sum(prices[-ma_2_interval:])/ma_2_interval    
     state = ma_1 > ma_2
+       
+    bollinger_std_upper = bollinger_ma_list[-1] + (bollinger_std_list[-1])*2
+    bollinger_std_lower = bollinger_ma_list[-1] - (bollinger_std_list[-1])*2
+    print(bollinger_std_upper, bollinger_std_lower)
+    bollinger_width = (bollinger_std_upper - bollinger_std_lower) / bollinger_ma_list[-1]
+    print(bollinger_width)
+    
+    if bollinger_width > 0.0026:
+        atis_serbest = True
+        print("Bollinger TRUE")
+    else:
+        atis_serbest = False
+        print("Bollinger FALSE")
+    
+    print(prices[-1])
     
     # add \nLast trades result: {5}% \nMax Drawdown till beginning: {6}%
-    if state != state_global:
+    if state != state_global and atis_serbest:
         if state:
             tradeCount = tradeCount+1
             wallet_at_buy = wallet_local
@@ -86,8 +117,10 @@ def algo_bot (update):
             updater.bot.send_message(chat_id="@bebelere_balon", 
                                      text= "Selling BTC! \nMoving Average {0} Days: {1} \nMoving Average {2} Days: {3} \nTotal value at transation time: {4} \nLast trade's gain: {5}% \nTotal gain: {6}"
                                      .format(ma_1_interval,round(ma_1,2),ma_2_interval,round(ma_2,2),wallet_local, win, total_eff))
+            #bot.send_animation(chat_id="@bebelere_balon", animation=)
+
     else:
-        updater.bot.send_message(chat_id="@bebelere_balon", text="No cross in BTC/USDT MA{0},MA{1}".format(ma_1_interval,ma_2_interval))
+        updater.bot.send_message(chat_id="@bebelere_balon", text="No cross or low bollinger width in BTC/USDT MA{0},MA{1}".format(ma_1_interval,ma_2_interval))
         win = (wallet_local-wallet_at_buy)/wallet_at_buy
         loss = (wallet_local-wallet_at_sell)
         if state:
@@ -99,7 +132,7 @@ def algo_bot (update):
     state_global = state
     wallet_global = wallet_local
 
-updater.job_queue.run_repeating(algo_bot, 1800, first=1800)
+updater.job_queue.run_repeating(algo_bot, 900, first=900)
 updater.start_polling()
 updater.idle()
                                      
